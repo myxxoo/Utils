21b595d00c40f5627003770b9675080ef15e3dcb
unknown <user@user-PC.(none)>
2012-08-10 15:54:39 +0800

    new File

---
+++ src/com/my/utils/HttpUtil.java
@@ -0,0 +1,12 @@
+package com.my.utils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import android.util.Log;
+
+public class HttpUtil {
+
+}
